c4eb623a94d0ae8ef3253794e3f1a87258d6567f
Bhaskar <bhaskaronlinenow@yahoo.com>
2014-06-16 23:11:54 -0500

    Data Processing

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 CleaningData
 ============
+1) Create data frame for the feature names by reading the features.txt
+2) Select the features for mean by using grep command on feature name
+2) Select the features for std by using grep command on feature name
+3) Join the mean and std feature names dataframe using rbind to create master list of features we are interested in. This DF will be used later
+4) Assign proper column names to above DF and sort by the feature ID
+5) Create DF for activity ID and Labels by reading activity_labels.txt. This DF will be used later
+6) Create DF for Test Oservation data(all features) by reading  X_test.txt. We will extract only the mean and std features later from this DF
+7) Create DF for Test Subject data by reading the subject_test.txt file. 
+8) Create DF for Activity Test data by reading the file y_test.txt
+9) Add ID column to Activity data in order to sort after merge operation
+10) Add Activity Labels to the Activity ID DF using merge command
+11) After merging, sort based on the ID added earlier.
+12) Merge using cbind all 3 data(Subject, Activity Labels and Observations) to create Master Data Set.
+13) Perform similar processing for the train dataset
+14) Join the Test and Train Data Sets using rbind to create Master Data Set
+15) Use the Feature Vector created earlier to subset the Master Data Set and select only the Mean and Std columns. 
+16) Put descriptive names for the columns using the feature DF created earlier. Also ensure putting names for "subject" and "activity" columns
+17) Calculate the mean of each observation for each subject and activity using the aggregate command grouped by subject and activity
+18) Return the tidy Data Set returned by the aggregate command above.
--- run_analysis.R
+++ run_analysis.R
@@ -2,29 +2,42 @@ run_analysis <- function(){
   
   ## creating data frame for the feature names
   df <- read.table("./features.txt",header=FALSE, sep=" ")
+  ## select the features for mean
   medf <- df[grep("mean()",df[,2], fixed=TRUE),]
+  ## select the features for std
   stddf <- df[grep("std()",df[,2], fixed=TRUE),]
+  ## create data frame combining mean and std using rbind
   alldf <- rbind(medf,stddf)
+  ## assign names to columns
   names(alldf) <- c("col","name")
   alldf <- alldf[with(alldf, order(col)),]
+  
+  ## read the activity ID and Labels
   alabdf <- read.table("./activity_labels.txt",header=FALSE, sep=" ")
   names(alabdf) <- c("col","activity")
   
+  ## read test observation data
   dattestdf <- read.table("./test/X_test.txt",header=FALSE)
+  ## read test subject data
   subtestdf <- read.table("./test/subject_test.txt",header=FALSE)
+  
+  ## read the Activity data
   analysistestdf <- read.table("./test/y_test.txt",header=FALSE)
   names(analysistestdf) <- c("col")
-  ## replace Analysis ID with Label
+  ## add ID column to Activity data
   analysistestdf$id  <- 1:nrow(analysistestdf)
+  ## Add Activity Labels to the Activity ID list using merge
   labeltestdf <- merge(analysistestdf, alabdf, by = "col")
   labeltestdf <- labeltestdf[order(labeltestdf$id), ]
+  ## Add the Label column to the Data Data Set
   alltestdf <- cbind(labeltestdf[,"activity"],subtestdf,dattestdf)
   
+  ##Perform similar processing for the train dataset
   dattrdf <- read.table("./train/X_train.txt",header=FALSE)
   subtrdf <- read.table("./train/subject_train.txt",header=FALSE)
   analysistrdf <- read.table("./train/y_train.txt",header=FALSE)  
   names(analysistrdf) <- c("col")
-  ## replace Analysis ID with Label
+  ## replace Activity ID with Label
   analysistrdf$id  <- 1:nrow(analysistrdf)
   labeltrdf <- merge(analysistrdf, alabdf, by = "col")
   labeltrdf <- labeltrdf[order(labeltrdf$id), ]
@@ -33,13 +46,17 @@ run_analysis <- function(){
   names(alltestdf) <- names(alltrdf)
   print(ncol(alltestdf))
   print(ncol(alltrdf))
+  ## Merge the two datasets using rbind
   alldatadf <- rbind(alltestdf,alltrdf)
+  
   ## adding 2 since I added 2 columns above
   alldf[,1] <- alldf[,1] + 2
-  tmpdf <- data.frame(col=1:2,name=c("analysis","subject"))
+  tmpdf <- data.frame(col=1:2,name=c("activity","subject"))
   newalldf <- rbind(tmpdf,alldf)
-  ## select only the means and std deviations
+  ## select only the means and std deviations columns
   alldatadf <- alldatadf[,newalldf[,1]]
+  ## Put descriptive names for the columns using the Feature Names dataframe
   names(alldatadf) <- newalldf[,2]
-  aggrDF <- aggregate(. ~ subject+analysis, alldatadf,mean)
+  ## Calculate the mean of each observation for each subject and activity
+  aggrDF <- aggregate(. ~ subject+activity, alldatadf,mean)
 }
